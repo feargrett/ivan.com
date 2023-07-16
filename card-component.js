5ac05d4d40c249a2740dc4e27e4d10e21b874ae8
agustin.casagna <agustin.casagna@sisorg.com.ar>
2023-07-16 15:56:07 -0300

    create a card component

---
+++ card-component.js
@@ -0,0 +1,49 @@
+export const cardComponent = function (element) {
+  
+  let _element = element;
+  let _name;
+  let _image;
+  let _location;
+  let _description;
+  let _healthPoints;
+
+  const component = () => `<div class="card">
+    <div class="card-image">
+      <div class="image">
+        <div class="image-container" style="background-image: url('${_image}');"></div>
+      </div>
+    </div>
+    <div class="card-content">
+      <div class="healt-container">
+        <span>&#10084 ${_healthPoints}</span>
+      </div>
+      <div class="media">
+        <div class="media-content has-text-centered">
+          <p class="title is-4">${_name}</p>
+          <p class="subtitle is-6">${_location}</p>
+        </div>
+      </div>
+      <div class="content">
+        <p>${_description}</p>
+      </div>
+    </div>
+  </div>`;
+
+  const refreshComponent = () => $(_element).innerHTML = component();
+  
+  const initialize = (name, image, location, description, healthPoints) => {
+    _name = name;
+    _image = image;
+    _location = location;
+    _description = description;
+    _healthPoints = healthPoints;
+     refreshComponent();
+  }
+
+  const setName = (name) => {
+    _name = name;
+    refreshComponent();
+  }
+  
+  return [initialize, setName];
+};
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import { $ } from './helper.js';
+import { cardComponent } from './card.js';
 
 const bosses = [];
+const [initializeCardOne] = cardComponent('#result_one');
+const [initializeCardTwo] = cardComponent('#result_two');
 
 const apiGetBosses = (limit = 100, page = 0) => {
   return fetch('https://eldenring.fanapis.com/api/bosses?limit=' + limit + '&page=' + page)
         .then(response => response.json());
 } 
+
+const getBoss = (id) => bosses.find(boss => boss.id === id);
+
+const setCard = (value, initializeFunction) => {
+  const boss = getBoss(value);
+  initializeFunction(boss.name, boss.image, boss.location, boss.description, boss.healthPoints);
+};
 
 const initialize = async () =>  {  
   const response = [...(await apiGetBosses(100,0)).data, ...(await apiGetBosses(100,1)).data];
@@ -15,48 +25,17 @@ const initialize = async () =>  {
     html += '<option value="' + boss.id +'" ' + (bosses.indexOf(boss) === 4 ? "selected" : "") + '>' + boss.name + '</option>';
   $('#select_one').innerHTML = html;
   $('#select_two').innerHTML = html;
-  setCard(bosses[4].id, '#result_one');
-  setCard(bosses[4].id, '#result_two');
+  setCard(bosses[4].id, initializeCardOne);
+  setCard(bosses[4].id, initializeCardTwo);
 }
-
-const card = (name, image, location, description, healthPoints) => `
-    <div class="card">
-      <div class="card-image">
-        <div class="image">
-          <div class="image-container" style="background-image: url('${image}');"></div>
-        </div>
-      </div>
-      <div class="card-content">
-        <div class="healt-container">
-          <span>&#10084 ${healthPoints}</span>
-        </div>
-        <div class="media">
-          <div class="media-content has-text-centered">
-            <p class="title is-4">${name}</p>
-            <p class="subtitle is-6">${location}</p>
-          </div>
-        </div>
-        <div class="content">
-          <p>${description}</p>
-        </div>
-      </div>
-    </div>
-`;
-
-const getBoss = (id) => bosses.find(boss => boss.id === id);
-
-const setCard = (value, element) => {
-  const boss = getBoss(value);
-  $(element).innerHTML = card(boss.name, boss.image, boss.location, boss.description, boss.healthPoints);
-};
 
 window.onload = async () => {
   await initialize();  
   $('#select_one').onchange = function() { 
-    setCard(this.value, '#result_one');
+    setCard(this.value, initializeCardOne);
   };
   $('#select_two').onchange = function() { 
-    setCard(this.value, '#result_two');
+    setCard(this.value, initializeCardTwo);
   };
 };
 
